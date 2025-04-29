ea87eaaccc97bd5aa1b438b9d2e416c721ec0fa5
Wainwain09 <157557971+Wainwain09@users.noreply.github.com>
2025-04-29 16:35:18 +0800

    ewan

--- react-fe-webapp/src/admin/login.jsx
+++ react-fe-webapp/src/admin/login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Login.css'; // <-- Important: connect the CSS
 
-function Login() {
+export default function login() {
   return (
     <div className="page">
       {/* Top Navigation Bar */}
--- react-fe-webapp/src/router.jsx
+++ react-fe-webapp/src/router.jsx
@@ -1,12 +1,10 @@
-import { createBrowserRouter, Navigate } from "react-router-dom";
+import {createBrowserRouter, Navigate} from "react-router-dom";
 import Login from "./views/login.jsx";
 import NotFound from "./views/notfound.jsx";
 import LayoutExample from "./components/layoutexample.jsx";
 
-import AdminDashboard from "./admin/pages/dashboard.jsx";
-import AdminLogin from "./admin/pages/login.jsx";
+const route = createBrowserRouter ( [ 
 
-const route = createBrowserRouter([
     {
         path: '/',
         element: <LayoutExample />,
@@ -20,19 +18,11 @@ const route = createBrowserRouter([
                 element: <Login />
             },
             {
-                path: '/admin/login',
-                element: <AdminLogin />
-            },
-            {
-                path: '/admin/dashboard',
-                element: <AdminDashboard />
-            },
-            {
                 path: '*',
                 element: <NotFound />
             }
         ]
     }
-]);
+])
 
 export default route;
