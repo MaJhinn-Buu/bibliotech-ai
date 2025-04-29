7a1a655e3e46dfcdc91cb30b47fd41e4d7a726b6
Wainwain09 <157557971+Wainwain09@users.noreply.github.com>
2025-04-29 16:45:12 +0800

    fixing

---
+++ react-fe-webapp/src/views/login.jsx
@@ -0,0 +1,13 @@
+export default function login() {
+
+    return (
+        <>
+        {/* This is just a placeholder for your stuff*/} 
+
+        <div>
+            <h1>LOGIN</h1>
+        </div>
+        </>
+    )
+
+}
